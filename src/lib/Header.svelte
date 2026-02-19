7cb5bc7ade36508ec45a231b55584f2c14b33293
rcarver3 <rcarver3@gatech.edu>
2026-02-19 01:54:51 -0500

    size changes

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,42 +6,69 @@
     import vgenLogo from "../assets/vgen_icon.png";
 </script>
 
-<img src={profilePic} class="logo profile" alt="Demonically Online" />
+<img src={profilePic} class="profile" alt="Demonically Online" />
 <h1>Demonically Online</h1>
-<a href="https://x.com/demoniclyonline" target="_blank" rel="noreferrer">
-    <figure>
-        <img src={twitterLogo} class="logo" alt="Twitter Logo" />
-        <figcaption>Twitter</figcaption>
-    </figure>
-</a>
-<a href="https://bsky.app/profile/demonically-online.bsky.social" target="_blank" rel="noreferrer">
-    <figure>
-        <img src={blueskyLogo} class="logo" alt="Bluesky Logo" />
-        <figcaption>Bluesky</figcaption>
-    </figure>
-</a>
-<a href="https://toyhou.se/demonically_online" target="_blank" rel="noreferrer">
-    <figure>
-        <img src={toyhouseLogo} class="logo" alt="Toyhouse Logo" />
-        <figcaption>Toyhouse</figcaption>
-    </figure>
-</a>
-<a href="https://vgen.co/demonically_online" target="_blank" rel="noreferrer">
-    <figure>
-        <img src={vgenLogo} class="logo" alt="VGen Logo" />
-        <figcaption>VGen</figcaption>
-    </figure>
-</a>
+<div class="logo-container">
+    <a href="https://x.com/demoniclyonline" target="_blank" rel="noreferrer">
+        <figure>
+            <img src={twitterLogo} class="logo" alt="Twitter Logo" />
+            <figcaption>Twitter</figcaption>
+        </figure>
+    </a>
+    <a
+        href="https://bsky.app/profile/demonically-online.bsky.social"
+        target="_blank"
+        rel="noreferrer"
+    >
+        <figure>
+            <img src={blueskyLogo} class="logo" alt="Bluesky Logo" />
+            <figcaption>Bluesky</figcaption>
+        </figure>
+    </a>
+    <a
+        href="https://toyhou.se/demonically_online"
+        target="_blank"
+        rel="noreferrer"
+    >
+        <figure>
+            <img src={toyhouseLogo} class="logo" alt="Toyhouse Logo" />
+            <figcaption>Toyhouse</figcaption>
+        </figure>
+    </a>
+    <a
+        href="https://vgen.co/demonically_online"
+        target="_blank"
+        rel="noreferrer"
+    >
+        <figure>
+            <img src={vgenLogo} class="logo" alt="VGen Logo" />
+            <figcaption>VGen</figcaption>
+        </figure>
+    </a>
+</div>
 
 <style>
+    .logo-container {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 0.25em;
+    }
+    figure {
+        margin: 0;
+        flex-shrink: 1;
+    }
     a {
         display: inline-flex;
         flex-direction: column;
+        align-items: center;
         text-decoration: none;
-        padding: 0;
     }
     .logo {
-        height: 8em;
+        max-height: 8em;
+        padding: 1.5em;
         will-change: transform;
         transition: transform 300ms ease-in-out;
         &:hover {
@@ -49,9 +76,23 @@
         }
     }
     .profile {
+        max-height: 10em;
         animation: float 4s ease-in-out infinite;
     }
 
+    @media (max-width: 768px) {
+        .logo {
+            max-height: 6em;
+            padding: 1em;
+        }
+    }
+
+    @media (max-width: 480px) {
+        .logo {
+            max-height: 4em;
+            padding: 0.5em;
+        }
+    }
     @keyframes float {
         0% {
             transform: translateY(0px);
