5ec428418e8b5ca0e8976c437e18de9708509354
rcarver3 <rcarver3@gatech.edu>
2026-03-23 02:16:36 -0400

    remove webp extension parsing code
    that is now done via Lambda, thank you god

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,10 +4,7 @@ const cdnUrl = "https://cdn.demonically-online.com/";
 export const load: PageServerLoad = async ({ fetch }) => {
     const res = await fetch(cdnUrl + "manifest.json");
     const artList = await res.json();
-    const artUrls = artList['files'].map((entry: string) => {
-        const filenameSplit = entry.split('.');
-        return `${cdnUrl}${filenameSplit[0] + '.webp'}`;
-    });
+    const artUrls = artList['files'].map((entry: string) => `${cdnUrl}${entry}`);
     const splitArt = artUrls.slice(0, 5);
 
     return { splitArt };
