dfae1262986afa7c105ac02ae1e9b28e4cc64b26
rcarver3 <rcarver3@gatech.edu>
2026-03-24 05:04:08 -0400

    create about me page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,15 @@
-<h1>About Me</h1>
+<h2 style="text-align: center">About Me</h2>
+<p>
+    I am Felix (he/him), a 20 year old trans man furry artist. I draw digitally
+    and traditionally, and I enjoy drawing demons, furries, people, animals,
+    faces, and anything colorful. My art style is cartoonish, textured,
+    colorful, and heavily inspired by JJBA.
+</p>
+<p>
+    I’ve been drawing ever since I could hold a colored pencil, but I really
+    started to lock in around 15, about the same time I came out as trans. My
+    parents called me demonic and said that demons were influencing me, so I
+    started out drawing exclusively demons as my subjects. The demons I draw
+    aren’t meant to be evil, they represent my feelings of being misunderstood
+    and looked down on. I hope that others can relate to them as well.
+</p>
