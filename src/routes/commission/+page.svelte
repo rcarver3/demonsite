bb8b0dcd2a729ff3db59cc574c5669fae48d153f
Rhett Carver <rhett.carver@gtri.gatech.edu>
2026-04-01 16:16:20 -0400

    update pricing and remove tff promo

--- src/routes/commission/+page.svelte
+++ src/routes/commission/+page.svelte
@@ -4,22 +4,13 @@
         You can message me through any of my linked socials to commission me. I
         take paypal, venmo, zelle, and cashapp.
     </p>
-    <p>
-        <b>Commissions come with a free bonus sketch during TFF!</b>
-    </p>
     <hr />
     <p>Will Draw: Furries, humans, animals, pretty much anything else :)</p>
     <p>Won’t Draw: NSFW (Artistic nudity is OK)</p>
     <p>I will not draw requests I am uncomfortable with!</p>
     <hr />
     <h3 style="text-align: center">Pricing</h3>
-    <ul>
-        <li>Sketch - $5</li>
-        <li>Headshot/pfp - $10</li>
-        <li>Bust/half body - $15-20</li>
-        <li>Full body - $25</li>
-        <li>Detailed background +$10</li>
-    </ul>
+    <p>All Commissions - $5</p>
 </div>
 
 <style>
