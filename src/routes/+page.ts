cbb9231b2ab5a66f6618c8bc4e230e12ce406ce7
Rhett Carver <rhett.carver@gtri.gatech.edu>
2026-06-21 21:28:02 -0400

    change static paths

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,8 @@
 import type { PageLoad } from "./$types";
 import { featuredImages } from "$lib/gallery-data";
-import { resolve } from "$app/paths";
 
 export const load: PageLoad = async () => {
-	const splitArt = featuredImages.map((img) => resolve("/[src]", {src: img.src}));
+  const splitArt = featuredImages.map((img) => `/${img.src}`);
 
-	return { splitArt };
+  return { splitArt };
 };
